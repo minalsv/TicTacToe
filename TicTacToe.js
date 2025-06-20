f51cae6b37081baffdb2c858eeb39949212c4742
minalsv <57864362+minalsv@users.noreply.github.com>
2025-06-20 18:48:49 +0200

    Minor comments

--- TicTacToe.js
+++ TicTacToe.js
@@ -14,13 +14,14 @@ export default function TicTacToe() {
   const { width, height } = useWindowDimensions();
   const isPortrait = height >= width;
 
-  const [gridContainerHeight, setGridContainerHeight] = useState(0);
+  const [gridContainerHeight, setGridContainerHeight] = useState(0); //I want to make the font size dynamic and dependent on the size of the grid container
 
   const gridSize = Math.min(width * 0.9, gridContainerHeight * 0.95); // 95% of available grid space
+  
   // 10% of the gridsize
   const dynamicFontSize = gridSize * 0.10; //(isPortrait ? 32 : 22);
 
-  //just for information, in final app remove or comment.
+  //just for information, in actual app remove or comment.
   console.log('gridContainerHeight: ', gridContainerHeight);
   console.log('width: ', width);
   console.log('gridSize: ', gridSize);
@@ -39,7 +40,9 @@ export default function TicTacToe() {
           setGridContainerHeight(event.nativeEvent.layout.height)
         }
       >
-        {gridContainerHeight > 0 && (
+        {gridContainerHeight > 0 && ( //for the first rendering, the grid container 
+        // size may be 0 due to app load timing mismatch and then the logic will fail, 
+        // so ignore first time rendering if the grid conatainer size is 0
           <View style={[styles.grid, { width: gridSize, height: gridSize }]}>
             {boardState.map((value, index) => {
               const row = Math.floor(index / 3);
